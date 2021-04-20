b661c3e9421a2add04ded209d09e739921c605ed
Dan Stanton <djstanton@gmail.com>
2021-04-20 09:23:14 -0700

    Solution correct

---
+++ src/com/dstanton/Ford.java
@@ -0,0 +1,25 @@
+package com.dstanton;
+
+public class Ford extends Car {
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return this.getClass().getSimpleName() + " -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return this.getClass().getSimpleName() + " -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return this.getClass().getSimpleName() + " -> brake()";
+    }
+
+
+
+}
